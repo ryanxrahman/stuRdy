a63db2c491e01426c7813d566efd21377978607b
rahscripts <rahmantjis@gmail.com>
2026-04-10 13:17:06 +0530

    register page done

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,14 +3,18 @@
 import { useActionState } from "react";
 import { registerAction } from "../login/actions";
 import BtnPrimary from "@/components/btn/BtnPrimary";
+import { ArrowLeftCircle } from "lucide-react";
 import Link from "next/link";
 
 export default function Register() {
     const [state, formAction, isPending] = useActionState(registerAction, null);
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen gap-4">
+        <div className="flex flex-col bg-neutral-900 items-center justify-center h-screen gap-4">
             <form action={formAction} className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <Link href="/">
+                    <legend className="flex gap-1 items-center link link-hover"><ArrowLeftCircle size={15} strokeWidth={1.5} /> back to home</legend>
+                </Link>
                 <legend className="fieldset-legend text-xl font-bold">Register</legend>
 
                 {state?.error && (
@@ -21,7 +25,7 @@ export default function Register() {
                 <input 
                     name="email"
                     type="email" 
-                    className="input w-full" 
+                    className="input w-full rounded-xl" 
                     placeholder="Email" 
                     required 
                 />
@@ -30,19 +34,19 @@ export default function Register() {
                 <input 
                     name="password"
                     type="password" 
-                    className="input w-full" 
+                    className="input w-full rounded-xl" 
                     placeholder="Password" 
                     required 
                 />
 
                 <div className="mt-4">
-                    <BtnPrimary type="submit" disabled={isPending}>
+                    <BtnPrimary className="w-full bg-neutral-900 text-white" type="submit" disabled={isPending}>
                         {isPending ? "Registering..." : "Register"}
                     </BtnPrimary>
                 </div>
                 
                 <div className="mt-2 text-center">
-                    <Link href="/login" className="link link-hover text-sm">
+                    <Link href="/login" className="link link-hover text-xs">
                         Already have an account? Login
                     </Link>
                 </div>
