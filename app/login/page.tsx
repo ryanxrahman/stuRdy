ab9cc07852c423370948c2625235e76bf4d481c2
rahscripts <rahmantjis@gmail.com>
2026-04-26 01:50:28 +0530

    /login page done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,53 +2,60 @@
 
 import { useActionState } from "react";
 import { loginAction } from "./actions";
-import BtnPrimary from "@/components/btn/BtnPrimary";
-import { ArrowLeft,ArrowLeftCircle } from "lucide-react";
+import { ArrowLeftCircle } from "lucide-react";
 import Link from "next/link";
+import BtnSecond from "@/components/btn/BtnSecond";
 
 export default function Login() {
     const [state, formAction, isPending] = useActionState(loginAction, null);
 
     return (
-        <div className="flex flex-col bg-neutral-900 items-center justify-center min-h-screen my-auto gap-4">
-            <form action={formAction} className="fieldset bg-slate-700 border-base-300 items-center justify-center my-atuo rounded-box w-md h-full border p-4 lg:p-10">
+       <main className="min-h-screen flex items-center justify-center">
+            <form action={formAction} className="bg-base-200 w-md max-md:w-sm h-full rounded-4xl outline-4 outline-base-300 p-6 py-10">
                 <div>
-                    <Link href="/">
-                        <legend className="flex gap-1 items-center link link-hover"><ArrowLeftCircle size={15} strokeWidth={1.5} /> back to home</legend>
+                    <Link className="text-xs" href="/">
+                        <p className="flex gap-1 items-center link link-hover hover:text-primary"><ArrowLeftCircle size={15} strokeWidth={1.5} /> back to home</p>
                     </Link>
-                    <div className="flex flex-col items-center mt-4 mb-2">
-                        <h1 className="text-xl font-bold">Welcome Back</h1>
-                        <p className="text-xs italic">lets study!</p>
+                    <div className="py-2 text-center  mb-6">
+                        <h1 className="text-2xl font-bold capitalize text-center">Welcome <span className="text-white bg-primary px-1">Back</span></h1>
+                        <p className="text-sm">lets study!</p>
                     </div>
                 </div>
 
                 {state?.error && (
-                    <div className="text-error text-sm mb-2">{state.error}</div>
+                    <div className="text-error text-center text-sm mb-4">{state.error}</div>
                 )}
 
-                <label className="label">Email</label>
-                <input 
-                    name="email"
-                    type="email" 
-                    className="input rounded-xl w-full focus:outline-none" 
-                    placeholder="zuck@gmail.com" 
-               
-                    required 
-                />
+                <div className="flex flex-col gap-2">
+                   <div>
+                       <label className="label">Email</label>
+                                       <input
+                        name="email"
+                        type="email"
+                        className="input focus:input-primary rounded-xl w-full "
+                        placeholder="zuck@gmail.com"
+                        required 
+                                       />
+                   </div>
 
-                <label className="label">Password</label>
-                <input 
-                    name="password"
-                    type="password" 
-                    className="input w-full rounded-xl" 
-                    placeholder="password" 
-                    required 
-                />
+                   <div>
+                       <label className="label">Password</label>
+                       <input
+                           name="password"
+                           type="password"
+                           className="input focus:input-primary rounded-xl w-full "
+                           placeholder="password"
+                           required
+                       />
+                   </div>
+
+                </div>
+               <div>
 
                 <div className="mt-4">
-                    <BtnPrimary className="w-full bg-neutral-900 text-white dark:bg-white dark:text-black" type="submit" disabled={isPending}>
+                    <BtnSecond className="w-full" type="submit" disabled={isPending}>
                         {isPending ? "Logging in..." : "Login"}
-                    </BtnPrimary>
+                    </BtnSecond>
                 </div>
 
                 <div className="mt-2 text-xs text-center">
@@ -56,7 +63,8 @@ export default function Login() {
                         No account? Register
                     </Link>
                 </div>
+                </div>
             </form>
-        </div>
+       </main>
     );
 }
